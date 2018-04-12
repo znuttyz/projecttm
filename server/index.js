93c740cc35c7c1d153b82a5e34ec1b8bd1e2cb1a
znuttyz <n.phannurat@gmail.com>
2018-04-12 23:07:16 +0700

    update server

--- server/index.js
+++ server/index.js
@@ -2,18 +2,13 @@ const express = require('express')
 const next = require('next')
 
 const port = 3000
-// const dev = process.env.NODE_ENV !== 'production'
-const dev = 'development'
+const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare()
   .then(() => {
     const server = express()
-
-    server.get('/test', (req, res) => {
-      res.send(200)
-    })
 
     server.get('*', (req, res) => {
       return handle(req, res)
