b48384c269e9dd42b37259adbb4f6b876db07078
oumho <oumho.p@gmail.com>
2018-04-30 01:09:29 +0700

    img add

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -1,14 +1,17 @@
 const items = ["หน้าแรก", "เกี่ยวกับเรา", "แบรนด์ของเรา", "ข่าวสาร/โปรโมชั่น", "คู่ค้าสัมพันธ์", "ติดต่อ"]
 
 const Nav = ({ isActive }) => (
-	<div>
+	<div className="header">
+		<div className="logo">
+			<img src="static/images/00-global/tummour.png" />
+		</div>
 		<div className="navbar">
 			<ul>
-				{items.map(item => 
-					<li className={isActive === item ? 'active' : ''} key={item} >
-						{item}
-					</li>
-				)}
+				<li><a href=""><img src="static/images/00-nav/01.png" /></a></li>
+				<li><a href=""><img src="static/images/00-nav/02.png" /></a></li>
+				<li><a href=""><img src="static/images/00-nav/03.png" /></a></li>
+				<li><a href=""><img src="static/images/00-nav/04.png" /></a></li>
+				<li><a href=""><img src="static/images/00-nav/05.png" /></a></li>
 			</ul>
 		</div>
 	</div>
